1839b90ceeb579ec837aa619d875633a6c8e691e
edwinwalela <edwinwalela@gmail.com>
2023-05-16 15:12:19 +0300

    validate flags

--- pkg/rage/rage.go
+++ pkg/rage/rage.go
@@ -2,24 +2,36 @@ package rage
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 type Rage struct {
 	URL      string
 	Method   string
-	BotCount uint
-	Attempts uint
+	BotCount int
+	Attempts int
 }
 
 func (r *Rage) LoadConfig() {
 	urlPtr := flag.String("url", "", "Target URL")
 	methodPtr := flag.String("method", "", "HTTP request method")
-	botCountPtr := flag.Uint("bots", 1, "Number of bots to spawn")
-	attemptsPtr := flag.Uint("attempts", 1, "Number of requests to make per user")
+	botCountPtr := flag.Int("bots", 1, "Number of bots to spawn")
+	attemptsPtr := flag.Int("attempts", 1, "Number of requests to make per user")
 	flag.Parse()
+
+	if *urlPtr == "" {
+		fmt.Printf("missing required -url flag\n")
+		os.Exit(2)
+	}
+	if *methodPtr == "" {
+		fmt.Printf("missing required -url flag\n")
+		os.Exit(2)
+	}
 
 	r.URL = *urlPtr
 	r.Method = *methodPtr
 	r.BotCount = *botCountPtr
 	r.Attempts = *attemptsPtr
+
 }
